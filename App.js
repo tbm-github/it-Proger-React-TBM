6aca99ed14bf67d946080a19df9bc3d1b465ecd6
Boris Tereshko <boris@Boriss-MacBook-Air.local>
2023-11-06 18:26:29 +0000

    init: react native/lesson #5

--- App.js
+++ App.js
@@ -1,56 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  Alert,
-  Image,
-  TouchableWithoutFeedback,
-} from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
-  const handleTextPress = () => console.log("Text press");
-  const handleButtonPress = () =>
-    Alert.alert("itTBM", "Main message", [
-      { text: "Да.", onPress: () => console.log("Yes button") },
-      { text: "Отмена", onPress: () => console.log("No button") },
-    ]);
-  const handleButtonPress2 = () =>
-    Alert.prompt("itTBM", "Main message", (text) => console.log(text));
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} style={styles.text} onPress={handleTextPress}>
-        {" "}
-        Hi my app! Go SafeAreaView. {"\n "} Привет.{" "}
-      </Text>
-      <Button
-        title="Нажми на меня"
-        color={"pink"}
-        onPress={handleButtonPress}
-      />
-      <Button title="Кнопка 2" color={"pink"} onPress={handleButtonPress2} />
-      <TouchableWithoutFeedback onPress={handleButtonPress}>
-        <Image
-          source={{
-            width: 200,
-            height: 150,
-            uri: "https://img.freepik.com/free-vector/hot-dog-street-snack-isolated-transparent_107791-18353.jpg",
-          }}
-        />
-      </TouchableWithoutFeedback>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <View></View>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  text: {
-    color: "red",
-  },
-});
+const styles = StyleSheet.create({});
