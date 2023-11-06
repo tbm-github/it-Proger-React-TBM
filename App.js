08e4141636ab6ea73a08bcd0226d9b9e11bb1d47
Boris Tereshko <boris@Boriss-MacBook-Air.local>
2023-11-06 17:24:36 +0000

    refactor: react native/lesson #3 formatted

--- App.js
+++ App.js
@@ -1,25 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, Button, Alert, Image, TouchableWithoutFeedback} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
-  const handleTextPress = () => console.log('Text press');
-  const handleButtonPress = () => Alert.alert ("itTBM", "Main message",[
-    {text: 'Да.', onPress: () => console.log('Yes button')},
-    {text: 'Отмена', onPress: () => console.log('No button')}
-  ]);
-  const handleButtonPress2 = () => Alert.prompt("itTBM", "Main message",text => console.log(text));
-  return ( 
+  const handleTextPress = () => console.log("Text press");
+  const handleButtonPress = () =>
+    Alert.alert("itTBM", "Main message", [
+      { text: "Да.", onPress: () => console.log("Yes button") },
+      { text: "Отмена", onPress: () => console.log("No button") },
+    ]);
+  const handleButtonPress2 = () =>
+    Alert.prompt("itTBM", "Main message", (text) => console.log(text));
+  return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} style={styles.text} onPress={handleTextPress}>  Hi my app! Go SafeAreaView. {'\n '} Привет. </Text>
-      <Button title='Нажми на меня' color={'pink'} onPress={handleButtonPress}/>
-      <Button title='Кнопка 2' color={'pink'} onPress={handleButtonPress2}/>
+      <Text numberOfLines={1} style={styles.text} onPress={handleTextPress}>
+        {" "}
+        Hi my app! Go SafeAreaView. {"\n "} Привет.{" "}
+      </Text>
+      <Button
+        title="Нажми на меня"
+        color={"pink"}
+        onPress={handleButtonPress}
+      />
+      <Button title="Кнопка 2" color={"pink"} onPress={handleButtonPress2} />
       <TouchableWithoutFeedback onPress={handleButtonPress}>
-<Image source={{
-  width:200,
-  height:150,
-  uri: "https://img.freepik.com/free-vector/hot-dog-street-snack-isolated-transparent_107791-18353.jpg"
-}}/>
-</TouchableWithoutFeedback>
+        <Image
+          source={{
+            width: 200,
+            height: 150,
+            uri: "https://img.freepik.com/free-vector/hot-dog-street-snack-isolated-transparent_107791-18353.jpg",
+          }}
+        />
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -28,9 +48,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   text: {
-    color: 'red'
+    color: "red",
   },
 });
