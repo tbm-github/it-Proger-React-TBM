b54a74e1dfa9f0682deac3b2d4cf4e3a27b8a8b2
Boris Tereshko <boris@Boriss-MacBook-Air.local>
2023-11-12 19:26:17 +0000

    init: start counter

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const handleButtonIncreasePress = (values) => {
+    values++;
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title={"-"} />
+      <Text numberOfLines={1} style={styles.text}>
+        0
+      </Text>
+      <Button title={"+"} onPress={{ handleButtonIncreasePress }} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "black",
   },
 });
