ddd834bd2e78391e653c56002b9368a94661cdd2
Boris Tereshko <boris@Boriss-MacBook-Air.local>
2023-11-13 16:29:59 +0000

    feat: count with useState

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const handleButtonIncreasePress = (values) => {
-    values++;
-  };
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Button title={"-"} />
-      <Text numberOfLines={1} style={styles.text}>
-        0
-      </Text>
-      <Button title={"+"} onPress={{ handleButtonIncreasePress }} />
-      <StatusBar style="auto" />
+      <View style={[styles.box, { backgroundColor: "yellow" }]}>
+        <Button title={"-"} onPress={() => setCount(count - 1)} />
+      </View>
+
+      <View style={[styles.box, { backgroundColor: "white" }]}>
+        <Text numberOfLines={1} style={styles.text}>
+          {count}
+        </Text>
+      </View>
+
+      <View style={[styles.box, { backgroundColor: "yellow" }]}>
+        <Button title={"+"} onPress={() => setCount(count + 1)} />
+      </View>
     </View>
   );
 }
@@ -23,9 +28,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  box: {
+    backgroundColor: "yellow",
+    width: "30%",
+    height: "auto",
+    borderWidth: 2,
+    borderColor: "black",
   },
   text: {
     color: "black",
+    textAlign: "center",
+    fontSize: 18,
+    textAlignVertical: "center",
   },
 });
