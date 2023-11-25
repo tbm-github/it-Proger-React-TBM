c0685fca9d578c9dcfe01d1b16f96305a8c8b2e4
Boris T <tbm_mail@tut.by>
2023-11-26 00:10:59 +0300

    feature: implement dropdown

---
+++ modules/taskManager/Filter.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { SelectList } from "react-native-dropdown-select-list";
+
+export const Filter = (props) => {
+  const [selected, setSelected] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <SelectList
+        setSelected={setSelected}
+        data={props.data}
+        onSelect={() => {
+          props.onSelect(selected);
+          console.log(selected);
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20,
+    width: "80%",
+    marginLeft: "10%",
+    borderWidth: 1,
+  },
+  input: {
+    padding: 10,
+    fontSize: 18,
+    color: "black",
+    textAlign: "left",
+    width: "80%",
+    borderWidth: 1,
+  },
+  btn: {
+    fontSize: 18,
+    color: "black",
+    borderRadius: 3,
+    backgroundColor: "red",
+    borderWidth: 1,
+  },
+});
--- modules/taskManager/index.js
+++ modules/taskManager/index.js
@@ -1,15 +1,59 @@
 import { useState } from "react";
 import { Text, View } from "react-native";
+import { Filter } from "./Filter";
 import { Todo } from "./Todo";
 import { Header } from "./Header";
 import { Form } from "./Form";
 import { StatusBar } from "expo-status-bar";
 
 const TaskManager = () => {
+  const data = [
+    { key: "all", value: "all" },
+    { key: "completed", value: "completed" },
+    { key: "notCompleted", value: "not-completed" },
+    { key: "delete", value: "delete" },
+  ];
+
+  const [showTodos, setShowTodos] = useState([
+    { title: "Task ", status: "completed", delete: "false", id: "0" },
+  ]);
+
+  const [filter, setFilter] = useState("all");
+
+  const onSelect = (filter) => {
+    setFilter(filter);
+    switch (filter) {
+      case "all":
+        const allShowTodos = todos.filter((todo) => todo.delete === "false");
+        setShowTodos(allShowTodos);
+        break;
+      case "completed":
+        const compShowTodos = todos.filter(
+          (todo) => todo.status === "completed"
+        );
+        setShowTodos(compShowTodos);
+        console.log("comp");
+        break;
+      case "notCompleted":
+        const notcompShowTodos = todos.filter(
+          (todo) => todo.status === "notCompleted"
+        );
+        setShowTodos(notcompShowTodos);
+        console.log("notComp");
+        break;
+      case "delete":
+        const delShowTodos = todos.filter((todo) => todo.delete !== "false");
+        setShowTodos(delShowTodos);
+    }
+    console.log(filter);
+    console.log(todos);
+    console.log(showTodos);
+  };
+
   const [todos, setTodos] = useState([
-    { title: "Task 1", status: "completed", id: "0" },
-    { title: "Task 2", status: "notCompleted", id: "1" },
-    { title: "Task 3", status: "delete", id: "2" },
+    { title: "Task 1", status: "completed", delete: "false", id: "0" },
+    { title: "Task 2", status: "notCompleted", delete: "false", id: "1" },
+    { title: "Task 3", status: "notCompleted", delete: "false", id: "2" },
   ]);
 
   const onAdd = (text) => {
@@ -19,15 +63,15 @@ const TaskManager = () => {
         {
           title: text,
           status: "notCompleted",
+          delete: "false",
           id: Math.random().toString(36).substring(7),
         },
       ];
     });
+    console.log(todos);
   };
 
   const onCheckbox = (id, isChecked) => {
-    console.log(id, " ", isChecked);
-
     setTodos((list) =>
       list.map((el) => {
         return el.id === id
@@ -35,7 +79,6 @@ const TaskManager = () => {
           : el;
       })
     );
-    console.log(todos);
   };
 
   const onRemove = (id) => {
@@ -46,10 +89,12 @@ const TaskManager = () => {
   return (
     <View>
       <Header />
+      <Filter data={data} onSelect={onSelect} />
       {todos.map((todo) => (
         <Todo
           key={todo.id}
           todo={todo}
+          filter={filter}
           onCheckbox={onCheckbox}
           onRemove={onRemove}
         />
